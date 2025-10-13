2726c1554250cefc53c08912f3eb81abaffe6adf
eduardo239 <eucrieiumaconta@gmail.com>
2025-10-13 20:26:09 -0300

    Add google_project_service resource for enabled APIs and remove unused local SSD disk configuration

--- envs/dev/api.tf
+++ envs/dev/api.tf
@@ -7,8 +7,4 @@ variable "enabled_apis" {
   ]
 }
 
-resource "google_project_service" "apis" {
-  for_each = toset(var.enabled_apis)
-  project  = var.project_id
-  service  = each.value
-}
+
--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,3 +1,8 @@
+resource "google_project_service" "apis" {
+  for_each = toset(var.enabled_apis)
+  project  = var.project_id
+  service  = each.value
+}
 
 resource "random_integer" "suffix" {
   min = 10000
