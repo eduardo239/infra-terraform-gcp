3b273d1bdb7623541a0fdb9ca0db092a655ceb28
eduardo239 <eucrieiumaconta@gmail.com>
2025-10-16 13:07:31 -0300

    Refactor variable definitions in variables.tf for improved readability and maintainability

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -1,17 +1,23 @@
-variable "project_id" { type = string }
-variable "region" { type = string }
-# Cloud Storage
-variable "bucket_name" { type = string }
-variable "location" { type = string }
-variable "force_destroy" { type = bool }
-# Compute Engine
-variable "compute_sa_account_id" { type = string }
-variable "instance_name" { type = string }
-variable "machine_type" { type = string }
-variable "zone" { type = string }
-# VPC Network
-variable "network_name" { type = string }
-variable "subnet_name" { type = string }
-variable "subnet_ip_cidr_range" { type = string }
-variable "firewall_name" { type = string }
-variable "firewall_target_tags" { type = list(string) }
+variable "project_id" {
+  type    = string
+  default = "proj-test-dev-474522"
+}
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+# # Cloud Storage
+# variable "bucket_name" { type = string }
+# variable "location" { type = string }
+# variable "force_destroy" { type = bool }
+# # Compute Engine
+# variable "compute_sa_account_id" { type = string }
+# variable "instance_name" { type = string }
+# variable "machine_type" { type = string }
+# variable "zone" { type = string }
+# # VPC Network
+# variable "network_name" { type = string }
+# variable "subnet_name" { type = string }
+# variable "subnet_ip_cidr_range" { type = string }
+# variable "firewall_name" { type = string }
+# variable "firewall_target_tags" { type = list(string) }
