890669f1e466e6029b2a57c6cb962e7ed1d7ea0d
eduardo239 <eucrieiumaconta@gmail.com>
2026-02-01 18:29:54 -0300

    Remove deprecated API and IAM configurations from Terraform files, including variable definitions for cloud resources, to streamline the codebase.

--- envs/dev/api.tf
+++
@@ -1,2 +0,0 @@
-# APIs são habilitadas via google_project_service em main.tf
-# Variável enabled_apis está em variables.tf
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -29,18 +29,3 @@ variable "enabled_apis" {
   ]
 }
 
-# # Cloud Storage
-# variable "bucket_name" { type = string }
-# variable "location" { type = string }
-# variable "force_destroy" { type = bool }
-# # Compute Engine
-# variable "compute_sa_account_id" { type = string }
-# variable "instance_name" { type = string }
-# variable "machine_type" { type = string }
-# variable "zone" { type = string }
-# # VPC Network
-# variable "network_name" { type = string }
-# variable "subnet_name" { type = string }
-# variable "subnet_ip_cidr_range" { type = string }
-# variable "firewall_name" { type = string }
-# variable "firewall_target_tags" { type = list(string) }
